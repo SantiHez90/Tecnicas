1f9addda98d4c7c853b66ff4e40e345488a366a4
SantiHez90 <santiago224z90@gmail.com>
2024-03-01 23:45:21 -0500

    Tarea 1

---
+++ Tarea #1/Variables.py
@@ -0,0 +1,11 @@
+#Variables
+estado = True
+nombre1 = "Santiago"
+edad1 = 22
+altura1 = 1.75
+pasaporte = True
+
+if pasaporte and edad1 >= 18:
+    print(nombre1, " Puede viajar")
+else:
+    print(nombre1, " No puede viajar")
